b1463b241a94c2372b1a4db2d0df774fb2236774
CodyNewton <Cody.newton-1@colorado.edu>
2026-01-20 14:58:29 -0700

    Add files via upload
    Template main file, as well as a mostly complete LoadASPENData. All I did for clean up was remove the NaN's I tried to figure out what else he wanted us to do but I was lost so there might be a bit more clean up that needs to be done. Other than that the LoadASPENData function works and outputs the data as required.

---
+++ LoadASPENData.m
@@ -0,0 +1,23 @@
+function [t_vec, av_pos_inert, av_att, tar_pos_inert, tar_att] =LoadASPENData(filename)
+% Reading in the data
+test=readmatrix(filename); 
+
+%% cleaning up the data 
+idx=find(isnan(test)); % finding all NaN values in the data
+[rowIdx, ~] = ind2sub(size(test), idx); % finding row idx of all NaN values in data 
+for i=1:length(rowIdx)
+test(rowIdx(i),:)=[]; % eliminating all NaN values in data
+end
+%% Parsing the data 
+Frame=test(:,1);
+time=Frame/100;
+t_vec=time';
+for i=1:3
+Target.HelicalAngles(:,i)=test(:,1+i);
+Target.Position(:,i)=test(:,4+i);
+Vehicle.HelicalAngles(:,i)=test(:,7+i);
+Vehicle.Position(:,i)=test(:,10+i);
+end
+[av_pos_inert, av_att, tar_pos_inert, tar_att] = ConvertASPENData(Vehicle.Position', Vehicle.HelicalAngles', Target.Position', Target.HelicalAngles');
+
+end
