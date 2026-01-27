6b68bdd7c111233d0c39ffe0b3aa254d1034bcf7
CodyNewton <Cody.newton-1@colorado.edu>
2026-01-27 10:23:49 -0700

    Add files via upload

--- LoadASPENData.m
+++ LoadASPENData.m
@@ -1,3 +1,34 @@
+% Cody Newton, Josiah Eubuen, Sovann Bonini, Jacob Johnson
+% ASEN 3801
+% LoadASPENData
+% Created 1/27/2026
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%Inputs: Data Files: '3801_sec001_Test1.csv','3801_sec001_Test2.csv','3801_sec001_Test3.csv'
+
+%Outputs: t_vec: 1 x 𝑛𝑛 time vector in seconds where 𝑛𝑛 = the total number of frames from the dataset.
+%           The frame rate of the camera is 100 Hz so the frame number can be converted to time in seconds.
+%         av_pos_inert: 3 x 𝑛𝑛 matrix of position vectors in meters for the aerospace vehicle in Frame 𝐸.
+%         av_att: 3 x 𝑛𝑛 matrix of attitude vectors listing the 3-2-1 Euler angles in radians for the
+%           aerospace vehicle relative to Frame 𝐸𝐸.
+%         tar_pos_inert: 3 x 𝑛𝑛 matrix of position vectors in meters for the target in Frame 𝐸𝐸.
+%         tar_att: 3 x 𝑛𝑛 matrix of attitude vectors listing the 3-2-1 Euler angles in radians for the
+%           target relative to Frame 𝐸𝐸
+% above copied from lab Documentation 
+
+
+%Methodology: Takes in the raw data checks for nan values to get rid of
+%them in the data set. Then seperates the raw data into useful categories
+%to then pass into ConvertASPENData and pass the outputs out. Also plots
+%the graph for question 3 as this is the easiest place to acess the data. 
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+
+
+
 function [t_vec, av_pos_inert, av_att, tar_pos_inert, tar_att] =LoadASPENData(filename)
 % Reading in the data
 test=readmatrix(filename); 
@@ -22,4 +53,15 @@ Vehicle.Position(:,i)=test(:,10+i);
 end
 [av_pos_inert, av_att, tar_pos_inert, tar_att] = ConvertASPENData(Vehicle.Position', Vehicle.HelicalAngles', Target.Position', Target.HelicalAngles');
 
+%% Question 3 Plotting
+figure(1);
+hold on 
+plot3(Vehicle.Position(:,1),Vehicle.Position(:,2),Vehicle.Position(:,3),'b',LineWidth=2 );
+plot3(Target.Position(:,1),Target.Position(:,2),Target.Position(:,3),'r--',LineWidth=2);
+legend('Drone Position','Target Position')
+xlabel('Position (mm)')
+ylabel('Postion (mm)')
+zlabel('Positon (mm)')
+title('Drone & Target Position (N Frame)')
+view(3);
 end
--- Main.m
+++ Main.m
@@ -1,3 +1,20 @@
+% Cody Newton, Josiah Eubuen, Sovann Bonini, Jacob Johnson
+% ASEN 3801
+% Main
+% Created 1/27/2026
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%Inputs: Data Files: '3801_sec001_Test1.csv','3801_sec001_Test2.csv','3801_sec001_Test3.csv'
+
+%Outputs: Various Figures 
+
+%Methodology: Takes in the raw data calls various functions to clean data
+%and process it into various frames using DCMS calculated from the given
+%data. 
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 clc; 
 clear; 
 close all;
@@ -8,24 +25,156 @@ files={'3801_sec001_Test1.csv','3801_sec001_Test2.csv','3801_sec001_Test3.csv'};
 % calling the load file that cleans the data and converts it to the
 % necessary format and units. The file names were put into a cell array for
 % ease of switching between the two
-[t_vec, av_pos_inert, av_att, tar_pos_inert, tar_att] =LoadASPENData(files{1});  
+[t_vec, av_pos_inert, av_att, tar_pos_inert, tar_att] =LoadASPENData(files{2});  
 
 
-
-figure();
+%%  Question 4
+% plotting
+figure(2)
 hold on 
-plot3(av_pos_inert(1,:),av_pos_inert(2,:),av_pos_inert(3,:));
-plot3(tar_pos_inert(1,:),tar_pos_inert(2,:),tar_pos_inert(3,:));
+subplot(3,1,1)
+sgtitle('Position Vs. Time (E-Frame)')
+plot(t_vec,av_pos_inert(1,:),LineWidth=2)
+xlabel('Time (s)')
+ylabel('X-Position (mm)')
+ylim([-2300 2000])
+subplot(3,1,2)
+
+
+plot(t_vec,av_pos_inert(2,:),LineWidth=2)
+xlabel('Time (s)')
+ylabel('Y-Position (mm)')
+legend('Drone Position',Location='SouthEast')
+ylim([-3000 300])
+subplot(3,1,3)
+
+
+plot(t_vec,av_pos_inert(3,:),LineWidth=2)
+xlabel('Time (s)')
+ylabel('Z-Position (mm)')
+ylim([-3000 200])
+
+figure(3)
+hold on 
+subplot(3,1,1)
+sgtitle('Angles Vs. Time (E-Frame)')
+plot(t_vec,av_att(1,:)*180/pi,LineWidth=2)
+xlabel('Time (s)')
+ylabel('Alpha (Deg)')
+
+subplot(3,1,2)
+
+
+plot(t_vec,av_att(2,:)*180/pi,LineWidth=2)
+xlabel('Time (s)')
+ylabel('Beta (Deg)')
+legend('Drone Attitude',Location='SouthEast')
+
+subplot(3,1,3)
+
+
+plot(t_vec,av_att(3,:)*180/pi,LineWidth=2)
+xlabel('Time (s)')
+ylabel('Gamma (Deg)')
+
+
+%% Question 5
+
+% Converting to 313 angles using the methodology explained in the lab
+% document
+for i=1:length(av_att)
+DCM=RotationMatrix321(av_att(1:3,i));
+attitude313(1:3,i) = EulerAngles313(DCM);
+end
+
+% plotting
+figure(4)
+hold on 
+subplot(3,1,1)
+sgtitle('Angle Vs. Time (313)')
+plot(t_vec,attitude313(1,:)*180/pi,LineWidth=2)
+xlabel('Time (s)')
+ylabel('Alpha (Deg)')
+
+subplot(3,1,2)
+
+
+plot(t_vec,attitude313(2,:)*180/pi,LineWidth=2)
+xlabel('Time (s)')
+ylabel('Beta (Deg)')
+legend('Drone Attitude',Location='NorthEast')
+ylim([-10 200])
+subplot(3,1,3)
+
+
+plot(t_vec,attitude313(3,:)*180/pi,LineWidth=2)
+xlabel('Time (s)')
+ylabel('Gamma (Deg)')
+
+%% Question 6
+
+Tar_rel_av=tar_pos_inert-av_pos_inert; % calculating the target relative position vector using the V_A-V_B=V_(B->A) 
+
+
+% plotting 
+figure(5)
+hold on 
+subplot(3,1,1)
+sgtitle('Relative Position Vs. Time (E-Frame)')
+plot(t_vec,Tar_rel_av(1,:),LineWidth=2)
+xlabel('Time (s)')
+ylabel('X-Position (mm)')
+
+subplot(3,1,2)
+
+
+plot(t_vec,Tar_rel_av(2,:),LineWidth=2)
+xlabel('Time (s)')
+ylabel('Y-Position (mm)')
+legend('Drone Position',Location='NorthEast')
+
+subplot(3,1,3)
+
+
+plot(t_vec,Tar_rel_av(3,:),LineWidth=2)
+xlabel('Time (s)')
+ylabel('Z-Position (mm)')
 
 
 
 
 
+%% Question 7
+
+%Converting the Relative Position Vector to B Frame using the DCM at that
+%time. 
+for i=1:length(av_att) 
+DCM=RotationMatrix321(av_att(1:3,i));
+Tar_rel_av_B(1:3,i)=DCM*Tar_rel_av(1:3,i);
+end
 
 
 
+%plotting 
+figure(6)
+hold on 
+subplot(3,1,1)
+sgtitle('Relative Position Vs. Time (B-Frame)')
+plot(t_vec,Tar_rel_av_B(1,:),LineWidth=2)
+xlabel('Time (s)')
+ylabel('X-Position (mm)')
+
+subplot(3,1,2)
 
 
+plot(t_vec,Tar_rel_av_B(2,:),LineWidth=2)
+xlabel('Time (s)')
+ylabel('Y-Position (mm)')
+legend('Drone Position',Location='NorthEast')
+
+subplot(3,1,3)
 
 
-
+plot(t_vec,Tar_rel_av_B(3,:),LineWidth=2)
+xlabel('Time (s)')
+ylabel('Z-Position (mm)')
